7e2058497783d69cfb278ec51b34e3aff2bcd228
Ernst-D <ernst.dzerevanko@gmail.com>
2023-11-03 17:55:56 +0400

    added circleci sdk and config

---
+++ ci-configs/linux.js
@@ -0,0 +1,43 @@
+const { Config, Workflow, executors, Job, commands } = require("@circleci/circleci-config-sdk");
+
+const linuxConfig = new Config();
+const apiTestsWorkflow = new Workflow("API tests with Cypress");
+
+linuxConfig.addWorkflow(apiTestsWorkflow);
+
+const linuxExecutor = new executors.DockerExecutor(
+  "cypress/browsers:node-18.15.0-chrome-106.0.5249.61-1-ff-99.0.1-edge-114.0.1823.51-1",
+  "small"
+);
+
+const apiTestsJob = new Job("run-api-tests", linuxExecutor);
+linuxConfig.addJob(apiTestsJob);
+
+apiTestsJob
+  .addStep(new commands.Checkout())
+  .addStep(
+    new commands.Run({
+      command: "npm install",
+      name: "Install packages",
+    })
+  )
+  .addStep(
+    new commands.Run({
+      command: "npx cypress info",
+      name: "Show Cypress info",
+    })
+  )
+  .addStep(
+    new commands.Run({
+      name: "Run API tests using Cypress",
+      command: "npx cypress run --spec cypress/tests/api/",
+    })
+  );
+
+apiTestsWorkflow.addJob(apiTestsJob);
+
+const yamlConfig = linuxConfig.stringify();
+
+linuxConfig
+  .writeFile("./.circleci/linux-api-tests.yml")
+  .then(() => console.log("Generated CircleCI config!"));
