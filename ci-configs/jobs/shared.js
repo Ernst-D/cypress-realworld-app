1d310fabf8fd9df27a91cc0970b3360318f4ab4b
Ernst-D <ernst.dzerevanko@gmail.com>
2023-11-06 19:48:50 +0400

    installing missing chrome dependency

--- ci-configs/jobs/shared.js
+++ ci-configs/jobs/shared.js
@@ -14,6 +14,10 @@ const commandInstallPackages = new commands.Run({
 
 const sharedJob = (jobName = "setup-project") =>
   new Job(jobName, dockerExecutor("18.12.1-browsers"), [
+    new commands.Run({
+      name: "install missing dependency",
+      command: "apt-get update -y && apt-get install -y libu2f-udev",
+    }),
     new commands.Checkout(),
     commandSetYarnClassic,
     commandInstallPackages,
