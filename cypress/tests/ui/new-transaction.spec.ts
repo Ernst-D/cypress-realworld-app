e572e5d25834a4400ae0f45ba18cef536cf8be27
Ernst-D <ernst.dzerevanko@gmail.com>
2024-01-05 13:20:48 +0400

    final clean up for specs

--- cypress/tests/ui/new-transaction.spec.ts
+++ cypress/tests/ui/new-transaction.spec.ts
@@ -6,24 +6,6 @@ describe("New Transaction", function () {
   });
 
   it("displays new transaction errors", function () {
-    cy.getBySelLike("new-transaction").click();
-    cy.wait("@allUsers");
-
-    cy.getBySelLike("user-list-item").contains(ctx.contact!.firstName).click({ force: true });
-
-    cy.getBySelLike("amount-input").type("43").find("input").clear().blur();
-    cy.get("#transaction-create-amount-input-helper-text")
-      .should("be.visible")
-      .and("contain", "Please enter a valid amount");
-
-    cy.getBySelLike("description-input").type("Fun").find("input").clear().blur();
-    cy.get("#transaction-create-description-input-helper-text")
-      .should("be.visible")
-      .and("contain", "Please enter a note");
-
-    cy.getBySelLike("submit-request").should("be.disabled");
-    cy.getBySelLike("submit-payment").should("be.disabled");
-    cy.visualSnapshot("New Transaction Errors with Submit Payment/Request Buttons Disabled");
   });
 
   it("submits a transaction payment and verifies the deposit for the receiver", function () {
